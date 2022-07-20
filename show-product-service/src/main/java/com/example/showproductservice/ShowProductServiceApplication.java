27038c82426b2b87275df744815769636317a334
ML-std <ML-std@github. com>
2022-07-20 11:33:39 +0300

    Initial commit

---
+++ show-product-service/src/main/java/com/example/showproductservice/ShowProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.showproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowProductServiceApplication.class, args);
+    }
+
+}
