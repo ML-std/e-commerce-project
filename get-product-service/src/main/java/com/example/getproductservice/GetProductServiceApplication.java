4fea13e19b83cc5f7c6de09b00d2434ad9f1663b
ML-std <ML-std@github. com>
2022-07-20 11:53:07 +0300

    Initial Commit

---
+++ get-product-service/src/main/java/com/example/getproductservice/GetProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.getproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetProductServiceApplication.class, args);
+    }
+
+}
---
+++ get-product-service/src/test/java/com/example/getproductservice/GetProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
